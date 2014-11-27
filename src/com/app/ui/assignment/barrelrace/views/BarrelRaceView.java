69583383f8f465e92a09bd5a2b9a058c1bfe8936
Sudharshan <sudhar.ti@gmail.com>
2014-11-26 19:40:50 -0600

    Refactor code, cleanup logic

--- src/com/app/ui/assignment/barrelrace/views/BarrelRaceView.java
+++ src/com/app/ui/assignment/barrelrace/views/BarrelRaceView.java
@@ -26,7 +26,13 @@ public class BarrelRaceView extends SurfaceView implements Runnable, OnTouchList
     private Horse horse;
     private Barrel barrel1, barrel2, barrel3;
     
-    private float x,y, barrel1X, barrel1Y, barrel2X, barrel2Y, barrel3X, barrel3Y;
+    private float x,y;
+    private float barrel1X, barrel1Y, barrel2X, barrel2Y, barrel3X, barrel3Y;
+    private float fence1StartX, fence1StartY, fence1StopX, fence1StopY;
+    private float fence2StartX, fence2StartY, fence2StopX, fence2StopY;
+    private float fence3StartX, fence3StartY, fence3StopX, fence3StopY;
+    private float fence4StartX, fence4StartY, fence4StopX, fence4StopY;
+    private float fence5StartX, fence5StartY, fence5StopX, fence5StopY;
     private float horseRadius, barrelRadius;
     boolean isTouched = false;
     
@@ -37,16 +43,73 @@ public class BarrelRaceView extends SurfaceView implements Runnable, OnTouchList
         super(context);
         // TODO Auto-generated constructor stub
         this.context = context;
-        x = width/2;
-        y = height - 50;
-        horseRadius = 25;
-        barrelRadius = 25;
+        initalizeCoordinates(width, height);
+        initializeObject();
         holder = getHolder();
         vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         bMedia = MediaPlayer.create(context, R.raw.barrel_hit);
         setOnTouchListener(this);
     }
 
+    private void initalizeCoordinates(float width, float height) {
+        // TODO Auto-generated method stub
+        x = width/2;
+        y = height-50;
+        horseRadius = 25;
+        barrelRadius = 25; 
+        
+        barrel1X = (width/2);
+        barrel1Y = height-225;
+        
+        barrel2X = (width/2)+250;
+        barrel2Y = 125;
+        
+        barrel3X = (width/2)-250;
+        barrel3Y = 125;
+        
+        fence1StartX = 30;
+        fence1StartY = 30;
+        fence1StopX = width-30;
+        fence1StopY = 30;
+        
+        fence2StartX = 30;
+        fence2StartY = 30;
+        fence2StopX = 30;
+        fence2StopY = height-80;
+        
+        fence3StartX = width-30;
+        fence3StartY = 30;
+        fence3StopX = width-30;
+        fence3StopY = height-80;
+        
+        fence4StartX = 30;
+        fence4StartY = height-80;
+        fence4StopX = (width/2)-50;
+        fence4StopY = height-80;
+        
+        fence5StartX = width-30;
+        fence5StartY = height-80;
+        fence5StopX = (width/2)+50;
+        fence5StopY = height-80;
+        
+    }
+
+    private void initializeObject() {
+        // TODO Auto-generated method stub
+        
+        horse = new Horse();
+        
+        fence1 = new Fence(context, fence1StartX, fence1StartY, fence1StopX, fence1StopY);
+        fence2 = new Fence(context, fence2StartX, fence2StartY, fence2StopX, fence2StopY);
+        fence3 = new Fence(context, fence3StartX, fence3StartY, fence3StopX, fence3StopY);
+        fence4 = new Fence(context, fence4StartX, fence4StartY, fence4StopX, fence4StopY);
+        fence5 = new Fence(context, fence5StartX, fence5StartY, fence5StopX, fence5StopY);
+        
+        barrel1 = new Barrel(context, barrel1X, barrel1Y, barrelRadius);
+        barrel2 = new Barrel(context, barrel2X, barrel2Y, barrelRadius);
+        barrel3 = new Barrel(context, barrel3X, barrel3Y, barrelRadius);
+    }
+    
     @Override
     public void run() {
         // TODO Auto-generated method stub
@@ -58,40 +121,19 @@ public class BarrelRaceView extends SurfaceView implements Runnable, OnTouchList
             canvas = holder.lockCanvas();
             canvas.drawColor(context.getResources().getColor(R.color.bg_color));
             
-            fence1 = new Fence(context);
-            fence2 = new Fence(context);
-            fence3 = new Fence(context);
-            fence4 = new Fence(context);
-            fence5 = new Fence(context);
+            fence1.draw(canvas);
+            fence2.draw(canvas);
+            fence3.draw(canvas);
+            fence4.draw(canvas);
+            fence5.draw(canvas);
             
-            barrel1 = new Barrel(context);
-            barrel2 = new Barrel(context);
-            barrel3 = new Barrel(context);
-            
-            horse = new Horse();
-
-            barrel1X = (getWidth()/2);
-            barrel1Y = getHeight()-225;
-            
-            barrel2X = (getWidth()/2) + 250;
-            barrel2Y = 125;
-            
-            barrel3X = (getWidth()/2) - 250;
-            barrel3Y = 125;
-            
-            fence1.draw(30, 30, getWidth()-30, 30, canvas);
-            fence2.draw(30, 30, 30, getHeight()-80, canvas);
-            fence3.draw(getWidth()-30, 30, getWidth()-30, getHeight()-80, canvas);
-            fence4.draw(30, getHeight()-80, (getWidth()/2)-50, getHeight()-80, canvas);
-            fence5.draw(getWidth()-30, getHeight()-80, (getWidth()/2)+50, getHeight()-80, canvas);
-            
-            barrel1.draw(barrel1X, barrel1Y, barrelRadius, canvas);
-            barrel2.draw(barrel2X, barrel2Y, barrelRadius, canvas);
-            barrel3.draw(barrel3X, barrel3Y, barrelRadius, canvas);
+            barrel1.draw(canvas);
+            barrel2.draw(canvas);
+            barrel3.draw(canvas);
             
             horse.draw(x, y, 20, canvas);
             
-            if(checkCollision()) {
+            if(collides()) {
                 handleCollision();
             }
             
@@ -104,42 +146,34 @@ public class BarrelRaceView extends SurfaceView implements Runnable, OnTouchList
         bMedia.start();
         if(vibrator.hasVibrator()) {
             vibrator.vibrate(50);
-        }
+        }   
     }
 
-    private boolean checkCollision() {
+    private boolean collides() {
         // TODO Auto-generated method stub
         
-        boolean collidesBarrel1 = (Math.pow((x - barrel1X), 2) + Math.pow((y - barrel1Y), 2)) 
-                <= Math.pow((horseRadius + barrelRadius), 2);  
-        boolean collidesBarrel2 = (Math.pow((x - barrel2X), 2) + Math.pow((y - barrel2Y), 2)) 
-                <= Math.pow((horseRadius + barrelRadius), 2);
-        boolean collidesBarrel3 = (Math.pow((x - barrel3X), 2) + Math.pow((y - barrel3Y), 2)) 
-                <= Math.pow((horseRadius + barrelRadius), 2); 
-        
-        if(collidesBarrel1) {
-            readjustHorse(barrel1X, barrel1Y);
-            return true;
-        } else if(collidesBarrel2) {
-            readjustHorse(barrel2X, barrel2Y);
-            return true;
-        } else if(collidesBarrel3) {
-            readjustHorse(barrel3X, barrel3Y);
-            return true;
+        if(checkCriteria(barrel1)) {
+            readjustHorse(barrel1);
+        } else if(checkCriteria(barrel2)) {
+            readjustHorse(barrel2);
+        } else if(checkCriteria(barrel3)) {
+            readjustHorse(barrel3);
+        } else {
+            return false;
         }
         
-        return false;
+        return true;
     }
 
-    private void readjustHorse(float bX, float bY) {
+    private void readjustHorse(Barrel barrel) {
         // TODO Auto-generated method stub
-        if(x > bX) {
+        if(x > barrel.getX()) {
             x = x + 1f;
         } else {
             x = x - 1f;
         }
         
-        if(y > bY) {
+        if(y > barrel.getY()) {
             y = y + 1f;
         } else {
             y = y - 1f;
@@ -147,6 +181,11 @@ public class BarrelRaceView extends SurfaceView implements Runnable, OnTouchList
         
     }
 
+    private boolean checkCriteria(Barrel barrel) {
+        return (Math.pow((x - barrel.getX()), 2) + Math.pow((y - barrel.getY()), 2)) 
+                <= Math.pow((horseRadius + barrelRadius), 2);
+    }
+    
     public void pause() {
         isThreadRunning = false;
         while(true) {
