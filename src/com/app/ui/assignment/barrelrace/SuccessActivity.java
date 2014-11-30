d9f484af4ecc4c139fd950068463f1b41f47c0ed
Sudharshan <sudhar.ti@gmail.com>
2014-11-30 05:20:25 -0600

    Enter name once game gets over

--- src/com/app/ui/assignment/barrelrace/SuccessActivity.java
+++ src/com/app/ui/assignment/barrelrace/SuccessActivity.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.ui.assignment.barrelrace.objects.Score;
 import com.app.ui.assignment.barrelrace.util.FileUtil;
@@ -19,6 +21,7 @@ public class SuccessActivity extends Activity implements OnClickListener {
 
     private Button buttonHome;
     private TextView textViewTime;
+    private EditText editTextName;
     private Long timeElapsed = 0L;
     
     private FileUtil fileUtil;
@@ -33,6 +36,7 @@ public class SuccessActivity extends Activity implements OnClickListener {
         
         buttonHome = (Button) findViewById(R.id.buttonHome);
         textViewTime = (TextView) findViewById(R.id.textViewTime);
+        editTextName = (EditText) findViewById(R.id.editTextName);
         buttonHome.setOnClickListener(this);
         
         fileUtil = new FileUtil();
@@ -46,35 +50,37 @@ public class SuccessActivity extends Activity implements OnClickListener {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-    
-        scoresList = fileUtil.fetchHighScores();
-        
-        scoresList.add(new Score("Test", timeElapsed));
-        
-        Collections.sort(scoresList, new ScoreComparator());
-        
-        int size = 10;
-        
-        if(scoresList.size() < 10) {
-            size = scoresList.size();
-        }
-        
-        for(int i=0;i<size;i++) {
-            strBuilder.append(scoresList.get(i).getName());
-            strBuilder.append(":");
-            strBuilder.append(scoresList.get(i).getScoreTime());
-            strBuilder.append(System.lineSeparator());
-        }
-        
-        fileUtil.writeToFile(strBuilder.toString());
     }
 
     @Override
     public void onClick(View v) {
         // TODO Auto-generated method stub
         switch(v.getId()) {
-        case R.id.buttonHome: 
-            finish(); 
+        case R.id.buttonHome:
+            if(editTextName.getText().toString().trim().equals("")) {
+                Toast.makeText(getApplicationContext(), "Enter Name to continue", Toast.LENGTH_SHORT).show();
+            } else {
+                scoresList = fileUtil.fetchHighScores();
+                scoresList.add(new Score(editTextName.getText().toString(), timeElapsed));
+                
+                Collections.sort(scoresList, new ScoreComparator());
+                
+                int size = 10;
+                
+                if(scoresList.size() < 10) {
+                    size = scoresList.size();
+                }
+                
+                for(int i=0;i<size;i++) {
+                    strBuilder.append(scoresList.get(i).getName());
+                    strBuilder.append(":");
+                    strBuilder.append(scoresList.get(i).getScoreTime());
+                    strBuilder.append(System.lineSeparator());
+                }
+                
+                fileUtil.writeToFile(strBuilder.toString());
+                finish(); 
+            }
             break;
         }
     }
